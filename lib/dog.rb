de0643b6036194c4814cd325d65ead25a9ec2499
shivani brijmohan <sbrij001@yahoo.com>
2018-11-23 22:08:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 # Your code goes here!
 class Dog
+  attr_accessor :name
   def initialize
-    @name
     @bark
+    @name
   end
-  def name=(name)
-    @name = name
+  def name
+    @name
   end
   def bark
     puts "woof!"
