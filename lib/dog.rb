bba178ed342e156868d68fd96bc505fc88dfa221
shivani brijmohan <sbrij001@yahoo.com>
2018-11-23 22:01:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,4 @@
 # Your code goes here!
 class Dog
 
-  def initialize
-    @name = name
-    @bark = bark
-  end
-  def name
-    @name
-  end
-  def bark
-    @bark = woof
-  end
-  def bark
-    @bark
-  end 
-end
+end 
